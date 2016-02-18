f5805215d7e6006e1ea1a76ac3b9ac97ec5fbde0
Ben <benjamin.achayanathan@cognizant.com>
2016-02-18 13:07:14 +0530

    Updated Code Changes.

--- NFV_Project/trydjango19/src/static/js/Login.js
+++ NFV_Project/trydjango19/src/static/js/Login.js
@@ -1,15 +1,56 @@
 ﻿function ASPvalidate() {
-    if ((document.getElementById("userName").value).length == 0) {
+    if ((document.getElementById("username").value).length == 0) {
         alert("UserName is Required");
-        document.getElementById("userName").focus();
+        document.getElementById("username").focus();
         return false;
-    } else if ((document.getElementById("Password").value).length == 0) {
+    } else if ((document.getElementById("password").value).length == 0) {
         alert("Password is Required");
-        document.getElementById("Password").focus();
+        document.getElementById("password").focus();
         return false;
     }
-    else { window.location = "Developer.html"; return true; }
+    else
+    /*var JSONonj = {"name": "James", "age": "25"};*/
+        $.post($("#frmlogin").attr("action"),
+            $("#frmlogin :input").serializeArray(),
+            function (data) {
+                if (data == "Developer") {
+                    alert("Dev hi");
+                    window.location = "Developer.html";
+                    return true;
+                }
+                else if (data == "admin") {
+                    alert("Admin hi");
+                    window.location = "/nfv/admin/";
+                    alert(window.location.pathname);
+                    return true;
+                }
+                else if (data == "Enterprise") {
+                    window.location = "Enterprise.html";
+                    return true;
+                }
+                else {
+                    window.location = "Invalid.html";
+                    return true;
+                }
+
+            });
 }
+
+  /*  $.ajax({
+        url: "/nfv/admin/",
+        type: "POST",
+        data: JSONonj,
+        dataType: "json",
+        success: function (data) {
+            alert('success');
+            alert(data);
+            /!*window.location.href = "/nfv/admin/";*!/
+        }
+
+
+    });
+}*/
+
 
 function redirect() {
     window.location = "Developer.html";
